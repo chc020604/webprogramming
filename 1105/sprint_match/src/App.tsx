4e862d867279966e050c929dd337f53a86f0c524
조현창 <109057035+chc020604@users.noreply.github.com>
2025-11-11 09:27:00 +0900

    Update App.tsx

--- 1105/sprint_match/src/App.tsx
+++ 1105/sprint_match/src/App.tsx
@@ -1,5 +1,5 @@
 //import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+//import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -16,7 +16,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
